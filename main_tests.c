6d1e36842d8dd94541cf31f0fd85a9cffe8b1ffb
Michiel Teerling <mteerlin@f0r4s18.codam.nl>
2020-11-28 19:07:29 +0100

    these changes conflict, so be aware

--- main_tests.c
+++ main_tests.c
@@ -6,7 +6,7 @@
 /*   By: mteerlin <mteerlin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/23 12:17:59 by mteerlin      #+#    #+#                 */
-/*   Updated: 2020/11/24 17:56:41 by mteerlin      ########   odam.nl         */
+/*   Updated: 2020/11/28 18:20:38 by mteerlin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,47 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include "get_next_line.h"
 
-int	get_next_line(int fd, char **line);
-
-int	main(void)
+typedef struct	s_fdlst
 {
-	char	*line;
-	int		ret1;
-	//int		ret2;
-	int		fd1;
-	//int		fd2;
+	int		fd;
+	int		ret;
+	s_fdlst	next;
+}				t_fdlst;
 
-	fd1 = open("test file", O_RDONLY);
-	//fd2 = open("ThisHasTextInIt", O_RDONLY);
-	ret1 = 1;
-	//ret2 = 1;
+int	main(int argc, char *argv[argc])
+{
+	size_t	cnt;
+	t_fdlst	*start;
+	t_fdlst	*temp;
+	char	*line;
+
 	line = NULL;
-	while (ret1 > 0)
+	if (argc < 2)
 	{
-		ret1 = get_next_line(fd1, &line);
-		if (ret1 > 0)
+		printf("\033[0;31m");
+		printf("No file path given.\n");
+		return (0);
+	}
+	else
+	{
+		start = (t_fdlst *)malloc(sizeof(t_fdlst));
+		if (!start)
+			return (NULL);
+		new->fd = open(argv[1], O_RDONLY);
+		new->ret = 1;
+		new->next = NULL;
+		temp = start;
+		cnt = 2;
+		while (cnt < argc)
 		{
-			printf("%s\n", line);
-			free(line);
-			line = NULL;
+			temp->next = (t_fdlst *)malloc(sizeof(t_fdlst));
+			temp = temp->next;
+			temp->fd = open(argv[cnt], O_RDONLY);
+			temp->ret = 1;
+			temp->next = NULL;
 		}
-	//	ret2 = get_next_line(fd2, &line);
-	//	if (ret2 > 0)
-	//	{
-	//		printf("%s\n", line);
-	//		free(line);
-	//		line = NULL;
-	//	}
 	}
 	return (0);
 }
